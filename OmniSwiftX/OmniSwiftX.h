6a3b0b8c7f3d2a5583376113d03620a638abe703
Cooper Knaak <cooperknaak@gmail.com>
2016-03-26 22:39:37 -0500

    Initial Commit

---
+++ OmniSwiftX/OmniSwiftX.h
@@ -0,0 +1,19 @@
+//
+//  OmniSwiftX.h
+//  OmniSwiftX
+//
+//  Created by Cooper Knaak on 3/26/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OmniSwiftX.
+FOUNDATION_EXPORT double OmniSwiftXVersionNumber;
+
+//! Project version string for OmniSwiftX.
+FOUNDATION_EXPORT const unsigned char OmniSwiftXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmniSwiftX/PublicHeader.h>
+
+
